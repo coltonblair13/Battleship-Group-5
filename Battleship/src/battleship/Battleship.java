2152cfc7e82ab6f8377a101f02964650486a9f97
BE$T <BE$T@10.17.161.50>
2014-05-08 21:09:58 -0600

    Battleship

--- Battleship/src/battleship/Battleship.java
+++ Battleship/src/battleship/Battleship.java
@@ -1,22 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package battleship;
 
-/**
- *
- * @author BE$T
- */
-public class Battleship {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+public class Battleship {
+    String name;
+    String instructions = "The object of Battleship is to sink all of your \n"
+                + "opponent's ships before they sink yours. You each get to \n"
+                + "choose where to place three ships on your board. You then \n"
+                + "take turns guessing locations on the board where you think \n"
+                + "your opponent's ships are. If you guess a location which \n"
+                + "contains part of a ship, it's a hit; otherwise, it's a miss.";
     public static void main(String[] args) {
-        // TODO code application logic here
+        Battleship myGame = new Battleship();
+        myGame.getName();
+        myGame.displayHelp();
+        Board boardSize = new Board();
+        boardSize.displaySize();
     }
     
+    public void getName() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter your name: ");
+        this.name = input.next();
+    }
+    
+    public void displayHelp() {
+        System.out.println("Welcome, " + this.name + "\n");
+        System.out.println(this.instructions);
+    }
 }
