7a46b972ad4c44dce8342af629119a14c5f43c5e
BE$T <BE$T@192.168.2.17>
2014-05-21 18:38:32 -0600

    Built HelpMenuView and HelpMenuControl classes

--- Battleship/src/battleship/HelpMenuControl.java
+++ Battleship/src/battleship/HelpMenuControl.java
@@ -11,5 +11,79 @@ package battleship;
  * @author Ethan Stewart
  */
 public class HelpMenuControl {
+    public HelpMenuControl() {
+        
+    } 
+
+    public void displayBoardHelp() {
+        System.out.println();
+        this.displayHelpBorder();             
+        System.out.println( 
+                "\tThe game board for Battleship is a 10X10 grid of squares."
+                + "\n\tPlayers place their ships in different locations. They then "
+                + "\n\tguess different locations in an attempt to find all of their "
+                + "\n\topponent's ships. The first one to \"sink\" all five of their "
+                + "\n\topponent's ships wins!");
+        displayHelpBorder();
+    }
     
+    
+        
+    public void displayGameHelp() {
+        System.out.println();
+        displayHelpBorder();     
+        System.out.println( 
+                 "\tThe objective of the game is to be the first player to sink "
+                 + "\n\tall of their opponent's ships is the winner. Each player"
+                 + "\n\ttakes a turn to guess a location on the board in hopes that "
+                 + "\n\tthey can hit part of a ship. First one to sink all five "
+                 + "\n\tof their opponent's ships wins."); 
+        displayHelpBorder();
+    }
+            
+    public void displayRealPlayerHelp() {
+        System.out.println();
+        displayHelpBorder();     
+        System.out.println( 
+                "\tA real player manually takes their turn by guessing "
+                + "\n\ta location on the board."); 
+        displayHelpBorder();
+    }
+    
+                   
+    public void displayComputerPlayerHelp() {
+        System.out.println();
+        displayHelpBorder();     
+        System.out.println( 
+                "\tA computer based player automatically guesses a square "
+                + "\n\timmediatly after a real player in a single player game."
+                ); 
+        displayHelpBorder();
+    }
+             
+    public void displayShipHelp() {
+        System.out.println();
+        displayHelpBorder();     
+        System.out.println( 
+               "\tEach player starts with five ships. One is five squares long, "
+               + "\n\tone is four, two are three, and one is two. Each guess "
+               + "\n\tcontaining part of a ship is a hit; when all parts of a ship "
+               + "\n\thave been hit, the ship is sunk."); 
+        displayHelpBorder();
+    }
+                 
+    public void displayHitsMissesHelp() {
+        System.out.println();
+        displayHelpBorder();
+        System.out.println(
+                "\tIf the square you guess contains part of an opponent's ship, "
+                + "\n\tthen it will dispay an X. If you miss it will display an O.");
+        displayHelpBorder();
+    }
+    
+    
+    public void displayHelpBorder() {       
+        System.out.println(
+        "\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    }
 }
--- Battleship/src/battleship/HelpMenuView.java
+++ Battleship/src/battleship/HelpMenuView.java
@@ -6,10 +6,83 @@
 
 package battleship;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Ethan Stewart
  */
 public class HelpMenuView {
+    private final static String[][] menuItems = {
+        {"B", "The board"},
+        {"C", "A computer player"}, 
+        {"G", "The game of Battleship"},
+        {"H", "Hits and Misses"},
+        {"R", "A regular player"},
+        {"S", "Ships"},
+        {"Q", "Quit Help"}        
+    };
     
+    // Create instance of the HelpMenuControl (action) class
+    private HelpMenuControl helpMenuControl = new HelpMenuControl();
+    
+    // Default constructor
+    public HelpMenuView() {
+        
+    } 
+    
+    // Display the help menu and get the end users input selection
+    public void getInput() {       
+              
+        String command;
+        Scanner inFile = new Scanner(System.in);
+        
+        do {
+            
+            this.display(); // Display the menu
+            
+            // Get command entered
+            command = inFile.nextLine();
+            command = command.trim().toUpperCase();
+            
+            switch (command) {
+                case "B":
+                    this.helpMenuControl.displayBoardHelp();
+                    break;
+                case "C":
+                    this.helpMenuControl.displayComputerPlayerHelp();
+                    break;
+                case "G":
+                    this.helpMenuControl.displayGameHelp();
+                    break;                  
+                case "H":
+                    this.helpMenuControl.displayHitsMissesHelp();
+                    break;
+                case "R":
+                    this.helpMenuControl.displayRealPlayerHelp();
+                    break;
+                 case "S":
+                    this.helpMenuControl.displayShipHelp();
+                    break; 
+                case "Q": 
+                    break;
+                default: 
+                    System.out.println("Invalid input. Please select an option from the menu.");
+                    continue;
+            }
+        } while (!command.equals("Q"));  
+        
+         return;
+    }
+
+        // displays the help menu
+    public final void display() {
+        System.out.println("\n\t===============================================================");
+        System.out.println("\tEnter the letter associated with one of the following commands:");
+
+        for (int i = 0; i < HelpMenuView.menuItems.length; i++) {
+            System.out.println("\t   " + menuItems[i][0] + "\t" + menuItems[i][1]);
+        }
+        System.out.println("\t===============================================================\n");
+    }
 }
