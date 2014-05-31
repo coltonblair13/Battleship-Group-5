febe30d0a9e95ed0488cd58e858278528519ded7
Colton Blair <coltonblair13@gmail.com>
2014-05-31 17:59:17 -0600

    This is the game menu view with changes that had to be made to game menu control

--- Battleship/src/battleship/Board.java
+++ Battleship/src/battleship/Board.java
@@ -79,7 +79,49 @@ public class Board {
             if((i + 1) % 10 == 0)
                 System.out.print("\n");  
         }
+    
     }
+    public static void showBoard(int[][] board){
+        System.out.println("\t1 \t2 \t3 \t4 \t5");
+        System.out.println();
+        
+        for(int row=0 ; row < 5 ; row++ ){
+            System.out.print((row+1)+"");
+            for(int column=0 ; column < 5 ; column++ ){
+                if(board[row][column]==-1){
+                    System.out.print("\t"+"~");
+                }else if(board[row][column]==0){
+                    System.out.print("\t"+"*");
+                }else if(board[row][column]==1){
+                    System.out.print("\t"+"X");
+                }
+                
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void initShips(int[][] ships){
+        Random random = new Random();
+        
+        for(int ship=0 ; ship < 3 ; ship++){
+            ships[ship][0]=random.nextInt(5);
+            ships[ship][1]=random.nextInt(5);
+            
+            //let's check if that shot was already tried 
+            //if it was, just finish the do...while when a new pair was randomly selected
+            for(int last=0 ; last < ship ; last++){
+                if( (ships[ship][0] == ships[last][0])&&(ships[ship][1] == ships[last][1]) )
+                    do{
+                        ships[ship][0]=random.nextInt(5);
+                        ships[ship][1]=random.nextInt(5);
+                    }while( (ships[ship][0] == ships[last][0])&&(ships[ship][1] == ships[last][1]) );
+            }
+            
+        }
+    }​
+
     
     
 }
